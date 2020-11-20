4d2030c81a5cd8398754cf0c5d08f6b9f973bc33
daler <dalerr@niddk.nih.gov>
2020-11-20 15:01:52 -0500

    include additional args in setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ requirements = [i.strip() for i in open('requirements.txt')]
 setup(
     name='termseq_peaks',
     version='0.1',
+    author='Ryan Dale',
+    author_email='dalerr@nih.gov',
     license='MIT',
     scripts=['bin/termseq_peaks'],
+    url='https://github.com/nichd-bspc/termseq-peaks',
     packages=['peaklib'],
     install_requires=requirements,
     long_description=open('README.rst').read(),

f81aa99cd275c753aef8a71eea96674adf7843bc
daler <dalerr@niddk.nih.gov>
2022-09-17 22:11:45 -0400

    add new annotation scripts

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 requirements = [i.strip() for i in open('requirements.txt')]
 setup(
     name='termseq_peaks',
-    version='0.1',
+    version='0.2',
     author='Ryan Dale',
     author_email='dalerr@nih.gov',
     license='MIT',
-    scripts=['bin/termseq_peaks'],
+    scripts=['bin/termseq_peaks', 'bin/class_annot', 'bin/table_output'],
     url='https://github.com/nichd-bspc/termseq-peaks',
     packages=['peaklib'],
     install_requires=requirements,
